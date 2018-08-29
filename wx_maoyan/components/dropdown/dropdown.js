ee4d9e7464822d4802abe0976d352949fa549098
chenchong-zc <2968408466@qq.com>
2018-08-29 09:14:22 +0800

    今天最后上传一次github

---
+++ wx_maoyan/components/dropdown/dropdown.js
@@ -0,0 +1,25 @@
+// components/dropdown/dropdown.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    json:Array,
+    screen: Array
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    self:'自营'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    
+  }
+  
+})
